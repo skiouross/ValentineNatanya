6e8184bfb24709fc8414eb79f80084e09b66f463
Kevin Celis <88560333+skiouross@users.noreply.github.com>
2026-02-13 20:49:23 -0600

    Update confirmation messages to Spanish

--- script.js
+++ script.js
@@ -54,16 +54,16 @@
 })();
 */
 const messages = [
-    "Are you sure?",
-    "Really sure??",
-    "Are you positive?",
+    "Estás segura?",
+    "Muy segura??",
+    "Segurisima?",
     "Pookie please...",
-    "Just think about it!",
-    "If you say no, I will be really sad...",
-    "I will be very sad...",
-    "I will be very very very sad...",
-    "Ok fine, I will stop asking...",
-    "Just kidding, say yes please! ❤️"
+    "Reconsideralo, un momento",
+    "Si dices que no voy a chillar",
+    "Voa a chillar como Frieren",
+    "A la de 3 lloro",
+    "Vale pues, dejare de preguntar ._.",
+    "Nah, mejor dí sí de una vez❤️"
 ];
 
 let messageIndex = 0;
